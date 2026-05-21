9d1e5de2c5111cf692c211d32d4edf11c48549ba
Miro Ali Akbar <113117782+Miro-Ali-Akbar@users.noreply.github.com>
2026-05-22 00:48:42 +0200

    added libmupdf crosscompiled for the 3ds. **should** work

--- source/main.c
+++ source/main.c
@@ -1,28 +1,29 @@
+#include <3ds.h>
+#include <mupdf/fitz.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <3ds.h>
 
-int main(int argc, char* argv[])
-{
-	gfxInitDefault();
-	consoleInit(GFX_TOP, NULL);
+int main(int argc, char *argv[]) {
+  gfxInitDefault();
+  consoleInit(GFX_TOP, NULL);
 
-	printf("Hello, world!\n");
+  // Restrict MuPDF to 16MB of RAM cache
+  fz_context *ctx = fz_new_context(NULL, NULL, 16 * 1024 * 1024);
+  printf("Hello, world!\n");
 
-	// Main loop
-	while (aptMainLoop())
-	{
-		gspWaitForVBlank();
-		gfxSwapBuffers();
-		hidScanInput();
+  // Main loop
+  while (aptMainLoop()) {
+    gspWaitForVBlank();
+    gfxSwapBuffers();
+    hidScanInput();
 
-		// Your code goes here
-		u32 kDown = hidKeysDown();
-		if (kDown & KEY_START)
-			break; // break in order to return to hbmenu
-	}
+    // Your code goes here
+    u32 kDown = hidKeysDown();
+    if (kDown & KEY_START)
+      break; // break in order to return to hbmenu
+  }
 
-	gfxExit();
-	return 0;
+  gfxExit();
+  return 0;
 }
